03faefcaabcdd8256fd361f7b6226341b0d6959d
serene1234 <S10268407@connect.np.edu.sg>
2025-01-19 18:16:49 +0800

    completed feature 4

--- S10268407E_PRG2Assignment/Program.cs
+++ S10268407E_PRG2Assignment/Program.cs
@@ -4,11 +4,14 @@
 // Partner Name	: Shawntrice Yip Yin Fei
 //==========================================================
 using S10268407E_PRG2Assignment;
+using System.Collections.Generic;
 
 //Feature 1
+//create airline and boarding gate dictionary
 Dictionary<string, Airline> airlineDict = new Dictionary<string, Airline>();
 Dictionary<string, BoardingGate> boardingGateDict = new Dictionary<string, BoardingGate>();
 
+//method to load data from airlines.csv and add objects to dictionary
 void CreateAirlines(Dictionary<string, Airline> airlineDict)
 {
     using (StreamReader sr = new StreamReader("airlines.csv"))
@@ -25,6 +28,7 @@ void CreateAirlines(Dictionary<string, Airline> airlineDict)
         }
     }
 }
+//method to load data from boardinggates.csv and add objects to dictionary
 void CreateBoardingGates(Dictionary<string, BoardingGate> bGateDict)
 {
     using (StreamReader sr = new StreamReader("boardinggates.csv"))
@@ -36,10 +40,22 @@ void CreateBoardingGates(Dictionary<string, BoardingGate> bGateDict)
         {
             string[] data = s.Split(',');
             string gateName = data[0];
-            bool supportsCFFT = Convert.ToBoolean(data[1]);
-            bool supportsDDJB = Convert.ToBoolean(data[2]);
+            bool supportsDDJB = Convert.ToBoolean(data[1]);
+            bool supportsCFFT= Convert.ToBoolean(data[2]);
             bool supportsLWTT = Convert.ToBoolean(data[3]);
-            bGateDict.Add(gateName, new BoardingGate(gateName, supportsCFFT, supportsDDJB, supportsLWTT));
+            bGateDict.Add(gateName, new BoardingGate(gateName, supportsDDJB, supportsCFFT, supportsLWTT));
         }
+    }
+}
+
+//Feature 4
+//method to display all boarding gates
+void DisplayAllBoardingGates(Dictionary<string, BoardingGate> bGateDict)
+{
+    Console.WriteLine("=============================================\r\nList of Boarding Gates for Changi Airport Terminal 5\r\n=============================================");
+    Console.WriteLine($"{"Gate Name",-16}{"DDJB",-23}{"CFFT",-23}{"LWTT",-23}");
+    foreach (var bGate in bGateDict.Values)
+    {
+        Console.WriteLine($"{bGate.GateName,-16}{bGate.SupportsDDJB,-23}{bGate.SupportsCFFT,-23}{bGate.SupportsLWTT,-23}");
     }
 }
