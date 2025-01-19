aa93eb040b91de0d729e03421af3bcc867862290
serene1234 <S10268407@connect.np.edu.sg>
2025-01-19 17:05:46 +0800

    made boardinggate class

---
+++ S10268407E_PRG2Assignment/BoardingGate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10268407E_PRG2Assignment
+{
+    class BoardingGate
+    {
+        public string GateName { get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
+        public bool SupportsLWTT { get; set; }
+        public Flight Flight { get; set; }
+        public BoardingGate(string gateName, bool supportsCFFT, bool supportsDDJB, bool supportsLWTT)
+        {
+            GateName = gateName;
+            SupportsCFFT = supportsCFFT;
+            SupportsDDJB = supportsDDJB;
+            SupportsLWTT = supportsLWTT;
+            Flight = null;
+        }
+        public double CalculateFees() 
+        {
+            return 0; 
+        }
+        public override string ToString()
+        {
+            return "Gate Name: " + GateName + "\tSupports CFFT: " + SupportsCFFT + "\tSupports DDJB: " + SupportsDDJB + "\tSupports LWTT: " + SupportsLWTT;
+        }
+    }
+}
