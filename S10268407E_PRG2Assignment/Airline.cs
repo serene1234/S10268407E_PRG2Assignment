79b0274ca521381e1bc844216ed11ec489a62ea4
serene1234 <S10268407@connect.np.edu.sg>
2025-01-19 16:55:00 +0800

    made airline class

---
+++ S10268407E_PRG2Assignment/Airline.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10268407E_PRG2Assignment
+{
+    class Airline
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Airline(string name, string code)
+        {
+            Name = name;
+            Code = code;
+            Flights = new Dictionary<string, Flight>();
+        }
+        public bool AddFlight(Flight flight)
+        {
+            if (!Flights.ContainsKey(flight.FlightNumber))
+            {
+                Flights.Add(flight.FlightNumber, flight);
+                return true;
+            }
+            return false;
+        }
+        public double CalculateFees()
+        {
+            double totalFees = 0;
+            foreach (var flight in Flights.Values)
+            {
+                totalFees += flight.CalculateFees();
+            }
+            return totalFees;
+        }
+        public bool RemoveFlight(Flight flight)
+        {
+            if (Flights.ContainsKey(flight.FlightNumber))
+            {
+                Flights.Remove(flight.FlightNumber);
+                return true;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            return "Airline: " + Name + "\tCode: " + Code + "\tFlights: " + Flights.Count;
+        }
+    }
+}
