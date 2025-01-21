2e5d37ef3322dc0f69e8911f29ad4f9f2541b2d7
Shawntricee <S10270521@connect.np.edu.sg>
2025-01-21 16:10:53 +0800

    completed the creation of classes

---
+++ S10268407E_PRG2Assignment/CFTTFlight.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10268407E_PRG2Assignment
+{
+    internal class CFTTFlight
+    {
+    }
+}
